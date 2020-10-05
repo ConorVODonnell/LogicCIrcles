e8efb8964bed0aee41158e2d0e8d2b0bd54a9e72
Conor <67388650+ConorVODonnell@users.noreply.github.com>
2020-10-05 14:51:39 -0400

    adds logic to detect nodes direction and turn player accordingly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public bool right;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!right)
+            transform.Rotate(Vector3.back * 180);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
-        transform.Rotate(Vector3.back * speed); // back is right, forward is left
+    }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DirectionalNode dn = other.GetComponent<DirectionalNode>();
+        Debug.Log(dn.current);
+
+        //check direction, default to zero
+        Vector3 dir = Vector3.zero;
+
+        if (dn.current == "right")
+            dir = Vector3.back;
+        else if (dn.current == "left")
+            dir = Vector3.forward;
+
+        transform.Rotate(dir * 90); // back is right, forward is left
     }
 }
